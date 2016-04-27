6ab23f8ae195d38902925f859ae87538ea900555
gonghaiming <364276231@qq.com>
2016-04-27 17:44:27 +0800

    test serized ok

--- Tools/serialized.c
+++ Tools/serialized.c
@@ -1,7 +1,9 @@
 
 #include "common.h"
 #include <string.h>
+#include <stdio.h>
 #include <arpa/inet.h>
+#define DEBUG
 /*****************************************************************
  * DESCRIPTION:
  *
@@ -13,12 +15,73 @@
  * RETURNS:
  *        NULL
  *****************************************************************/
-INT16 Serialized(char *pktdata, pkt *package)
+INT16 clientSerialized(char *pktdata,pktcontent **pcontent)
 {
-    INT16 pktLength = package->head.lengthPkt;
-    package->head.lengthPkt = htons(package->head.lengthPkt);
-    memcpy(pktdata, package, pktLength);
-    return pktLength;
+    
+    pktHeader header;
+    INT8 * pos = pktdata;
+    
+    header.code = 1;
+    header.id = 1;
+    memset(header.md5Auth, 0, 16);
+    
+    INT16 TotalVLength = 0;
+    
+    for( int i = 0; i < 2 ; i++){
+        TotalVLength += pcontent[i]->lengthItem;
+    }
+    
+    header.lengthPkt = TotalVLength + 20;
+    
+    header.lengthPkt = htons(header.lengthPkt);
+    
+    memcpy(pktdata, &header, 20);
+    pos += 20;
+    for( int i = 0; i < 2 ; i++){
+        memcpy(pos,pcontent[i],pcontent[i]->lengthItem);
+        pos += pcontent[i]->lengthItem;
+    }
+    
+    
+    
+    
+    return TotalVLength + 20;
+    
+}
+
+INT16 serverSerialized(char *pktdata,pktcontent *pcontent){
+    pktHeader header;
+    INT8 *pos = pktdata;
+    INT8 type  = pcontent->type;
+    memset(header.md5Auth, 0, 16);
+    switch (type) {
+        case 2:
+            header.code = 2;
+            header.id = 1;
+            header.lengthPkt = pcontent->lengthItem + 20;
+            header.lengthPkt = htons(header.lengthPkt);
+            memcpy(pktdata, &header, 20);
+            pos += 20;
+            memcpy(pos, pcontent, pcontent->lengthItem);
+            
+            break;
+        case 3:
+            header.code = 3;
+            header.id = 1;
+            header.lengthPkt = pcontent->lengthItem + 20;
+            header.lengthPkt = htons(header.lengthPkt);
+            memcpy(pktdata, &header, 20);
+            pos += 20;
+            memcpy(pos, pcontent, pcontent->lengthItem);
+            break;
+            
+        default:
+            printf("erro header type");
+            break;
+    }
+    
+    
+    return 20 + pcontent->lengthItem;
     
 }
 
